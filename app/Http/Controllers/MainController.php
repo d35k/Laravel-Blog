1d9d6e838085b86e448fff81b53b354b3852c6a7
Göktuğ Hatipoğlu <goktughatipoglu@yandex.com>
2017-06-04 17:17:50 +0300

    Ana sayfa ve Yazı detay düzenlendi
    Ana sayfaya Pagination eklendi ve Post detay sayfası düzenlendi

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\posts;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
 	public function index()
 	{
-		return view('pages.index');
+        $posts = posts::orderBy('id', 'DESC')->paginate(5);
+		return view('pages.index', compact( 'posts'));
 	}
+	public function article($url = null){
+	    $article = posts::where('url', $url)->first();
+	    if($article->count()){
+            return view('pages.article', compact('article'));
+        }
+        return redirect('/');
+    }
 }
