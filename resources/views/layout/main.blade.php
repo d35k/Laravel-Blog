c7871d4a25ecf272719d29a56addbf95f5f68748
Göktuğ Hatipoğlu <goktughatipoglu@yandex.com>
2017-06-04 17:19:14 +0300

    URL'ler düzeltildi.
    {{ url() }} kullanmadığım için bir çok yerde css ve img problemi
    çıkıyordu, bunu giderdim

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -8,15 +8,19 @@
     <title>@yield('title', 'Blog') | Goktug Hatipoglu</title>
     {{-- Başlık için alan belirtiliyor, eğer girilmesse Blog kalacağı söyleniyor. --}}
 
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
     <link rel="shortcut icon" href="images/Logo_40.png" type="image/x-icon">
     <!--Layui-->
-    <link href="plug/layui/css/layui.css" rel="stylesheet" />
+    <link href="{{ url('plug/layui/css/layui.css') }}" rel="stylesheet" />
     <!--font-awesome-->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
-    <link href="css/global.css" rel="stylesheet" />
-
-    <link href="css/home.css" rel="stylesheet" />
+    <link href="{{ url('css/global.css') }}" rel="stylesheet" />
+    <link href="{{ url('css/prettify.css') }}" rel="stylesheet" />
+    <link href="{{ url('css/home.css') }}" rel="stylesheet" />
+    <link href="{{ url('css/detail.css') }}" rel="stylesheet" />
 </head>
 <body>
     {{-- Navbar include ediliyor. --}}
@@ -30,7 +34,7 @@
             <div class="blog-main-right">
                 <div class="blogerinfo shadow">
                     <div class="blogerinfo-figure">
-                        <img src="images/author.jpg" alt="Author" />
+                        <img src="{{ url('images/author.jpg') }}" alt="Author" />
                     </div>
                     <p class="blogerinfo-nickname">Goktug Hatipoglu</p>
                     <p class="blogerinfo-introduce">Junior Developer</p>
@@ -58,10 +62,10 @@
 </footer>
 
 <!-- layui.js -->
-<script src="plug/layui/layui.js"></script>
+<script src="{{ url('plug/layui/layui.js') }}"></script>
 
-<script src="js/global.js"></script>
+<script src="{{ url('js/global.js') }}"></script>
 
-<script src="js/home.js"></script>
+<script src="{{ url('js/home.js') }}"></script>
 </body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,35 +1,34 @@
 @extends('layout.main')
-@section('title')
 @section('content')
 
     @include('layout.canvas')
     <div class="blog-container">
         <div class="blog-main">
 
-
             <div class="blog-main-left">
+
+                @foreach($posts as $item)
                 <div class="article shadow">
                     <div class="article-left">
-                        <img src="images/cover/201703181909057125.jpg" alt="Example"/>
+                        <img src="{{ url($item->image) }}" alt="{{ $item->title }}"/>
                     </div>
                     <div class="article-right">
                         <div class="article-title">
-                            <a href="javascript:void(0)">Example</a>
+                            <a href="{{ url('a/'. $item->url) }}">{{ $item->title }}</a>
                         </div>
                         <div class="article-abstract">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid consequatur deleniti
-                            deserunt esse et, magnam placeat porro praesentium vero. Aut deleniti natus nostrum. Aliquam
-                            ipsa necessitatibus nisi reiciendis sit.
+                            {!!  $item->content !!}
                         </div>
                     </div>
                     <div class="clear"></div>
                     <div class="article-footer">
-                        <span><i class="fa fa-clock-o"></i>&nbsp;&nbsp;2017-03-18</span>
-                        <span class="article-author"><i class="fa fa-user"></i>&nbsp;&nbsp;Absolutely</span>
-                        <span class="article-viewinfo"><i class="fa fa-eye"></i>&nbsp;0</span>
-                        <span class="article-viewinfo"><i class="fa fa-commenting"></i>&nbsp;4</span>
+                        <span><i class="fa fa-clock-o"></i>&nbsp;&nbsp;{{ $item->created_at }}</span>
+                        <span class="article-author"><i class="fa fa-user"></i>&nbsp;&nbsp;{{ $item->author }}</span>
+                        <span class="article-viewinfo"><i class="fa fa-eye"></i>&nbsp;{{ $item->views }}</span>
                     </div>
                 </div>
+                @endforeach
+                {{ $posts->links() }}
             </div>
 
 @endsection
