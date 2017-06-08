308488c96ad42ea845f90e256ea8d1f18e93a40c
Göktuğ Hatipoğlu <goktughatipoglu@yandex.com>
2017-06-08 23:30:03 +0300

    Post Model'inde düzenlemeler
    Post modelindeki yaptıklarıma küçük açıklamalar ekledim ve yorum
    özelliği için bir ekleme yaptım

--- app/posts.php
+++ app/posts.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class posts extends Model
 {
+    //Tablomun adını özel olarak belirtiyorum çünkü
+    //Laravel otomatik olarak ismin sonuna 's' takısı ekliyor.
     protected $table = "posts";
+    //Post'un yorumu var mı diye kontrol ettiriyorum.
+    public function comments()
+    {
+        return $this->hasMany(Comments::class, 'post_id', 'post_id');
+    }
 }
