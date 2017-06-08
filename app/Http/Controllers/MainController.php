edfa46044eb04fd3f835f114a61400237e4a0b10
Göktuğ Hatipoğlu <goktughatipoglu@yandex.com>
2017-06-08 23:28:13 +0300

    Main Controller'de küçük düzenlemeler
    Ana kontrol dosyamda post'u bulamazsa onun üstünde post bulmaya
    çalıştığını farkettim, bu yüzden onu if'in içine aldım ve yorum ekledim

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\posts;
 use Illuminate\Http\Request;
 
@@ -9,14 +8,20 @@ class MainController extends Controller
 {
 	public function index()
 	{
-        $posts = posts::orderBy('id', 'DESC')->paginate(5);
+	    //Postları sondan-başa olarak 5'li olarak ayırıyorum ve ana sayfaya yolluyorum
+        $posts = posts::orderBy('post_id', 'DESC')->paginate(5);
 		return view('pages.index', compact( 'posts'));
 	}
 	public function article($url = null){
+        //Gelen yazının url'inden yazıyı buluyorum
 	    $article = posts::where('url', $url)->first();
+	    //eğer yazı varsa yorumuyla beraber fırlatıyorum
 	    if($article->count()){
-            return view('pages.article', compact('article'));
+            //gelen yazının yorumunu alıyorum
+            $comments = $article->comments;
+            return view('pages.article', compact('article', 'comments'));
         }
+        //yoksa ana sayfaya dönücek :))
         return redirect('/');
     }
 }
