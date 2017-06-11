5e0b89d9b6113cc09a32f68a417b42d69aaf4ad2
Göktuğ Hatipoğlu <goktughatipoglu@yandex.com>
2017-06-12 00:00:13 +0300

    Moderation için Comments tabloma ekleme
    Comments tablosuna Moderation için bir iki column

---
+++ database/migrations/2017_06_11_191211_add_comments_moderation_columns.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCommentsModerationColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->smallInteger('status')->default(0);
+            $table->dateTime('moderated_at')->nullable();
+            //If you want to track who moderated the Model add 'moderated_by' too.
+            //$table->integer('moderated_by')->nullable()->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comments', function(Blueprint $table)
+        {
+            $table->dropColumn('status');
+            $table->dropColumn('moderated_at');
+            //$table->dropColumn('moderated_by');
+        });
+    }
+}
