1a87b656f93698074ffb0de8dabcf659cd6da4f1
Göktuğ Hatipoğlu <goktughatipoglu@yandex.com>
2017-06-11 23:57:30 +0300

    laravel-moderation için Model Ayarlandı
    Yorumları yönetici izinle yapacağım için, Laravel Moderation paketini
    özelliklerini ekledim

--- app/Comments.php
+++ app/Comments.php
@@ -4,8 +4,17 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+//Moderatable için use
+use Hootlex\Moderation\Moderatable;
+
 class Comments extends Model
 {
+    // Moderatable durumu ile yorumları onaylayıp/onaylanmayacağı özelliği ekleniyor
+    use Moderatable;
+
+    protected $table = "comments";
+
+    //Doldurulabilecek alanlar
     protected $fillable = [
         'post_id', 'content', 'author',
     ];
